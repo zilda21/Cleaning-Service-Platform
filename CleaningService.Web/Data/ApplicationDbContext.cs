a0ff9bdef0468f53adaf198e465c92d7f4fd8e6f
zilda21 <164346657+zilda21@users.noreply.github.com>
2026-01-02 14:41:45 +0200

    Identity .Net Core (login,Signup) is Added

---
+++ CleaningService.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CleaningService.Web.Models;
+
+namespace CleaningService.Web.Data
+{
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {}
+    
+         public DbSet<User> Users { get; set; }
+
+    
+}
+
+}
