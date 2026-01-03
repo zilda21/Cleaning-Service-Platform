a09a351a410964ca6cecb5ce1f9d768045349d80
zilda21 <164346657+zilda21@users.noreply.github.com>
2026-01-03 19:05:47 +0200

    Controle Panel is Setted Up to run the application with the Connection string (DB) as well Implmenting the entity framework

--- CleaningService.Web/Program.cs
+++ CleaningService.Web/Program.cs
@@ -1,9 +1,26 @@
+using CleaningService.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Razor Pages to the container
+builder.Services.AddRazorPages();
+
+// Register EF Core with connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register your services (BookService, etc.)
+// builder.Services.AddScoped<IBookService, BookService>();
+
+var app = builder.Build();
+
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,7 +37,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 // app.MapStaticAssets();
-// app.MapRazorPages()
+app.MapRazorPages();
 //    .WithStaticAssets();
 
 app.Run();
